69d1e40a7306d80079bed90ca96500ffb0383f8a
André Cardoso <andrexcardoso@gmail.com>
2022-10-17 11:11:29 -0300

    Base OpenAPI/Swagger configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="0.1",
+ *     title="BACKEND CART API",
+ *     description="Morsum challenge"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
